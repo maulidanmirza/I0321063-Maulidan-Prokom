ffd2f06dba9240f595acdd9fbbd4db08e434d987
unknown <maulidanmrz@gmail.com>
2022-05-20 10:15:36 +0700

    mengerjakan exercise-10

---
+++ Exercise-07/quiz.py
@@ -0,0 +1,7 @@
+Number1 = int(input("masukkan angka pertama: "))
+Number2 = int(input("masukkan angka kedua: "))
+Perkalian = Number1 * Number2
+if Perkalian > 100:
+    print(Perkalian, "perkalian tsb memiliki hasil lbh dr 100")
+else
+print(Perkalian, "Perkalian tsb memiliki hasil kurang dr 100")
